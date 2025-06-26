a12d5b470d9b6f76edc77e742d5a2dd0b81fb71b
Gabriel Rodrigues Tenório <164562713+gabrielrodriguestenorio@users.noreply.github.com>
2025-06-26 19:43:43 -0300

    Update Empoleon.java

--- Empoleon.java
+++ Empoleon.java
@@ -1,22 +1,28 @@
-public class Empoleon extends Pokemon{
-    
+public class Empoleon extends Pokemon {
     String movimento1;
     String movimento2;
     String movimento3;
     String movimento4;
 
-    public Empoleon() {
-
+    public Empoleon(){
         super();
     }
 
     public Empoleon(String apelido, Integer numero_pokedex, String especie, Integer geracao, String movimento1, String movimento2, String movimento3, String movimento4) {
-
         super(apelido, numero_pokedex, especie, geracao);
-
-        this.movimento1 = movimento1
-        this.movimento1 = movimento2
-        this.movimento1 = movimento3
-        this.movimento1 = movimento4
+        this.movimento1 = movimento1;
+        this.movimento2= movimento2;
+        this.movimento3= movimento3;
+        this.movimento4= movimento4;
+    }
+    @Override
+    public String getapelido(){
+        return apelido;
+    }
+    public void SetMovimento1(){
+        this.movimento1 = movimento1;
+        this.movimento2 = movimento2;
+        this.movimento3 = movimento3;
+        this.movimento4 = movimento4;
     }
 }
