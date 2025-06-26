b23cf868ee3f0ce69117093ec8f7fa27254189ff
Gabriel Rodrigues Tenório <164562713+gabrielrodriguestenorio@users.noreply.github.com>
2025-06-26 19:44:00 -0300

    Update Garchomp.java

--- Garchomp.java
+++ Garchomp.java
@@ -1,3 +1,28 @@
-public class Garchomp {
-    
+public class Garchomp extends Pokemon {
+    private String movimento1;
+    private String movimento2;
+    private String movimento3;
+    private String movimento4;
+
+    public Garchomp(){
+        super();
+    }
+
+    public Garchomp(String apelido, Integer numero_pokedex, String especie, Integer geracao, String movimento1, String movimento2, String movimento3, String movimento4) {
+        super(apelido, numero_pokedex, especie, geracao);
+        this.movimento1 = movimento1;
+        this.movimento2 = movimento2;
+        this.movimento3 = movimento3;
+        this.movimento4 = movimento4;
+    }
+    @Override
+    public String getapelido(){
+        return apelido;
+    }
+    public void SetMovimento1(){
+        this.movimento1 = movimento1;
+        this.movimento2 = movimento2;
+        this.movimento3 = movimento3;
+        this.movimento4 = movimento4;
+    }
 }
